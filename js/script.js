73ba2963ab3fe2e6f3e2632d68546e5e79af942e
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-03-22 17:00:45 +0000

    Did all the work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Ryan Duffet
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-function buttonClicked() {
-alert("Hello, World!")
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = base * height / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
 }
